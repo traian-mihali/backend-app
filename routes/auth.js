3164cd9bc111add7dff1462a1a67d6c172c5ede8
Traian <traianmihali696@gmail.com>
2019-07-30 02:05:32 +0300

    remove the Joi min length validation of the auth route

--- routes/auth.js
+++ routes/auth.js
@@ -21,12 +21,10 @@ router.post("/", async (req, res) => {
 function validate(req) {
   const schema = {
     email: Joi.string()
-      .min(5)
       .max(255)
       .required()
       .email(),
     password: Joi.string()
-      .min(5)
       .max(255)
       .required()
   };
